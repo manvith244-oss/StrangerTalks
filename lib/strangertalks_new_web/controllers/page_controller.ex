2b85b2f996a31f885f0dd0de9e0a2190cbb04922
Manvith <manvithh24@gmail.com>
2026-08-24 22:35:43 +0530

    fix(team10): preserve app entry contract in wrapper markup

--- lib/strangertalks_new_web/controllers/page_controller.ex
+++ lib/strangertalks_new_web/controllers/page_controller.ex
@@ -2,7 +2,7 @@ defmodule StrangertalksNewWeb.PageController do
   use StrangertalksNewWeb, :controller
 
   @app_script ~s(<script type="module" src="/assets/app.js?v=20260807_v2"></script>)
-  @expression_script ~s(<script type="module" src="/assets/expression_runtime.mjs?v=20260824_v1"></script>)
+  @expression_script ~s(<script type="module" src="/assets/expression_runtime.mjs?v=20260824_v1" data-app-entry="/assets/app.js?v=20260807_v2"></script>)
   @expression_css ~s(<link rel="stylesheet" href="/assets/expression_surface.css?v=20260824_v1">)
 
   def home(conn, _params) do
