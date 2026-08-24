2076c6cdcec01ce07be98572f3708cccbe336b4f
Manvith <manvithh24@gmail.com>
2026-08-24 22:58:23 +0530

    fix(team10): remove runtime html script surgery

--- lib/strangertalks_new_web/controllers/page_controller.ex
+++ lib/strangertalks_new_web/controllers/page_controller.ex
@@ -1,20 +1,9 @@
 defmodule StrangertalksNewWeb.PageController do
   use StrangertalksNewWeb, :controller
 
-  @app_script ~s(<script type="module" src="/assets/app.js?v=20260807_v2"></script>)
-  @expression_script ~s(<script type="module" src="/assets/expression_runtime.mjs?v=20260824_v1" data-app-entry="/assets/app.js?v=20260807_v2"></script>)
-  @expression_css ~s(<link rel="stylesheet" href="/assets/expression_surface.css?v=20260824_v1">)
-
   def home(conn, _params) do
-    html =
-      :strangertalks_new
-      |> Application.app_dir("priv/static/index.html")
-      |> File.read!()
-      |> String.replace("</head>", @expression_css <> "\n  </head>", global: false)
-      |> String.replace(@app_script, @expression_script, global: false)
-
     conn
     |> put_resp_content_type("text/html")
-    |> send_resp(200, html)
+    |> send_file(200, Application.app_dir(:strangertalks_new, "priv/static/index.html"))
   end
 end
