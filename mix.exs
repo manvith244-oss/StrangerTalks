891cc7dcfb939df24eded12b6b7fe978f4596d5e
Manvith <manvithh24@gmail.com>
2026-08-26 19:57:15 +0530

    ci: make precommit formatting verification-only

--- mix.exs
+++ mix.exs
@@ -82,7 +82,12 @@ defmodule StrangertalksNew.MixProject do
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
-      precommit: ["compile --warnings-as-errors", "deps.unlock --unused", "format", "test"],
+      precommit: [
+        "compile --warnings-as-errors",
+        "deps.unlock --unused",
+        "format --check-formatted",
+        "test"
+      ],
       "assets.deploy": ["phx.digest"]
     ]
   end
