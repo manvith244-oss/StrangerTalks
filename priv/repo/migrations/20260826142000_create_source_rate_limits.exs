c181eef9885cbc362a4462812befdcc47bca1fbe
Manvith <manvithh24@gmail.com>
2026-08-26 19:59:01 +0530

    fix(team1): add durable pre-identity abuse perimeter

---
+++ priv/repo/migrations/20260826142000_create_source_rate_limits.exs
@@ -0,0 +1,23 @@
+defmodule StrangertalksNew.Repo.Migrations.CreateSourceRateLimits do
+  use Ecto.Migration
+
+  def change do
+    create table(:source_rate_limits, primary_key: false) do
+      add :source_fingerprint, :binary, null: false
+      add :bucket, :string, null: false
+      add :window_key, :bigint, null: false
+      add :count, :integer, null: false
+      add :expires_at_ms, :bigint, null: false
+    end
+
+    create unique_index(:source_rate_limits, [:source_fingerprint, :bucket, :window_key],
+             name: :source_rate_limits_window_unique
+           )
+
+    create index(:source_rate_limits, [:expires_at_ms])
+
+    create constraint(:source_rate_limits, :source_rate_limits_count_positive,
+             check: "count > 0"
+           )
+  end
+end
