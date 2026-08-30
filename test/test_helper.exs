c6634186cd1d42e2c4ce703b29c2919692a56af6
Manvith <manvithh24@gmail.com>
2026-08-30 13:13:38 +0530

    test(ai): expose client info logs in test harness

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -2,4 +2,6 @@ ExUnit.start()
 
 Code.ensure_loaded!(StrangertalksNew.TurnCredentialTestClient)
 
+Logger.put_module_level(StrangertalksNew.AIService.Client, :info)
+
 Ecto.Adapters.SQL.Sandbox.mode(StrangertalksNew.Repo, :manual)
