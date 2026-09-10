7180df3daba17daaa54c125dd0cda7da055e4876
Manvith <manvithh24@gmail.com>
2026-09-11 00:54:45 +0530

    fix(security): close public schema to Data API roles

--- priv/repo/migrations/20260910192317_close_public_schema_data_api_boundary_rpr.exs
+++ priv/repo/migrations/20260910192317_close_public_schema_data_api_boundary_rpr.exs
@@ -1,7 +1,25 @@
 defmodule StrangertalksNew.Repo.Migrations.ClosePublicSchemaDataApiBoundaryRpr do
   use Ecto.Migration
 
-  def change do
+  @api_roles ~w(anon authenticated service_role)
 
+  def up do
+    execute("REVOKE ALL PRIVILEGES ON SCHEMA public FROM PUBLIC")
+
+    Enum.each(@api_roles, fn role ->
+      execute("""
+      DO $$
+      BEGIN
+        IF EXISTS (SELECT 1 FROM pg_roles WHERE rolname = '#{role}') THEN
+          EXECUTE 'REVOKE ALL PRIVILEGES ON SCHEMA public FROM #{role}';
+        END IF;
+      END
+      $$;
+      """)
+    end)
+  end
+
+  def down do
+    raise "RPR public-schema Data API boundary is intentionally irreversible; restore a captured pre-change database state instead of reopening schema access"
   end
 end
