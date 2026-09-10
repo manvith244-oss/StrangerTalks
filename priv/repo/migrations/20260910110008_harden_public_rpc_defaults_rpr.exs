2a0fac27edde322e3aa11e5377e00431c55e2aba
Manvith <manvithh24@gmail.com>
2026-09-10 16:31:37 +0530

    fix(security): close public RPC execute defaults

--- priv/repo/migrations/20260910110008_harden_public_rpc_defaults_rpr.exs
+++ priv/repo/migrations/20260910110008_harden_public_rpc_defaults_rpr.exs
@@ -1,7 +1,41 @@
 defmodule StrangertalksNew.Repo.Migrations.HardenPublicRpcDefaultsRpr do
   use Ecto.Migration
 
-  def change do
+  @api_roles ~w(anon authenticated service_role)
 
+  def up do
+    execute("REVOKE EXECUTE ON ALL FUNCTIONS IN SCHEMA public FROM PUBLIC")
+
+    execute("""
+    DO $$
+    BEGIN
+      EXECUTE format(
+        'ALTER DEFAULT PRIVILEGES FOR ROLE %I IN SCHEMA public REVOKE EXECUTE ON FUNCTIONS FROM PUBLIC',
+        current_user
+      );
+    END
+    $$;
+    """)
+
+    Enum.each(@api_roles, fn role ->
+      execute("""
+      DO $$
+      BEGIN
+        IF EXISTS (SELECT 1 FROM pg_roles WHERE rolname = '#{role}') THEN
+          EXECUTE 'REVOKE EXECUTE ON ALL FUNCTIONS IN SCHEMA public FROM #{role}';
+
+          EXECUTE format(
+            'ALTER DEFAULT PRIVILEGES FOR ROLE %I IN SCHEMA public REVOKE EXECUTE ON FUNCTIONS FROM #{role}',
+            current_user
+          );
+        END IF;
+      END
+      $$;
+      """)
+    end)
+  end
+
+  def down do
+    raise "RPR public RPC default closure is intentionally irreversible; restore a captured pre-change database state instead of reopening Data API function execution"
   end
 end
