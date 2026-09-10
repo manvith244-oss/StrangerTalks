f1a48625c0c79bdba26068de2142f4e6c49b26b2
Manvith <manvithh24@gmail.com>
2026-09-11 02:40:00 +0530

    security: close Living Thread pilot Data API surface

---
+++ priv/repo/migrations/20260911001100_secure_living_thread_pilot.exs
@@ -0,0 +1,36 @@
+defmodule StrangertalksNew.Repo.Migrations.SecureLivingThreadPilot do
+  use Ecto.Migration
+
+  @pilot_tables ~w(
+    living_thread_experiment_assignments
+    living_thread_pilot_events
+    living_threads
+  )
+
+  @api_roles ~w(anon authenticated service_role)
+
+  def up do
+    Enum.each(@pilot_tables, fn table ->
+      execute("ALTER TABLE public.#{table} ENABLE ROW LEVEL SECURITY")
+      revoke_table_api_access(table)
+    end)
+  end
+
+  def down do
+    raise "Living Thread pilot security closure is intentionally irreversible; restore a captured pre-change database state instead of reopening Data API access"
+  end
+
+  defp revoke_table_api_access(table) do
+    Enum.each(@api_roles, fn role ->
+      execute("""
+      DO $$
+      BEGIN
+        IF EXISTS (SELECT 1 FROM pg_roles WHERE rolname = '#{role}') THEN
+          EXECUTE 'REVOKE ALL PRIVILEGES ON TABLE public.#{table} FROM #{role}';
+        END IF;
+      END
+      $$;
+      """)
+    end)
+  end
+end
