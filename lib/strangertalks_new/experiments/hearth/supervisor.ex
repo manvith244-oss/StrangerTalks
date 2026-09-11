044cb64deb8cf8816496fee160c1536e0ffa001f
Manvith <manvithh24@gmail.com>
2026-09-11 19:05:19 +0530

    feat: wire Hearth lifecycle notifier into isolated supervisor

--- lib/strangertalks_new/experiments/hearth/supervisor.ex
+++ lib/strangertalks_new/experiments/hearth/supervisor.ex
@@ -1,7 +1,7 @@
 defmodule StrangertalksNew.Experiments.Hearth.Supervisor do
   use Supervisor
 
-  alias StrangertalksNew.Experiments.Hearth.Authority
+  alias StrangertalksNew.Experiments.Hearth.{Authority, Notifier}
 
   def start_link(opts \\ []) do
     Supervisor.start_link(__MODULE__, opts, name: __MODULE__)
@@ -9,6 +9,13 @@ defmodule StrangertalksNew.Experiments.Hearth.Supervisor do
 
   @impl true
   def init(_opts) do
-    Supervisor.init([{Authority, name: Authority}], strategy: :one_for_one)
+    Supervisor.init(
+      [
+        {Authority,
+         name: Authority,
+         notifier: Notifier}
+      ],
+      strategy: :one_for_one
+    )
   end
 end
